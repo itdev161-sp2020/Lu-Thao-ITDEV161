e70fb55933e5d29be9db639b68aac5da4c07a2eb
Lu-Thao <thaol31@gmatc.matc.edu>
2020-03-22 14:44:14 -0500

    add jwt

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 //initialize express application
 const app = express();
@@ -67,7 +69,22 @@ app.post(
 
                 //save to db and return
                 await user.save();
-                res.send('User successfully registered');
+
+                const payload ={
+                    user:{
+                        id: user.id 
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    {expiresIn: '10hr'},
+                    (err, token) => {
+                        if(err) throw err;
+                        res.json({token: token});
+                    }
+                );
             }catch(error){
                 res.status(500).send('Server error');
             }
